a5adf395f33ed7194b21227031c2bfbc982ce8e7
Priyanshu Kumar <priyanshukumarveeru5689@gmail.com>
2023-10-02 00:34:31 +0530

    Add files via upload

---
+++ switch/basic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int day;
+    printf("Enter the day :\n");
+    scanf("%d",&day);
+
+    switch(day){
+        case 1:printf("Monday");
+            break;
+        case 2:printf("Tuesday");
+            break;
+        case 3:printf("Wednesday");
+            break;
+        case 4:printf("Thrusday");
+            break;
+        case 5:printf("Friday");
+            break;
+        case 6:printf("Saturday");
+            break;
+        case 7:printf("Sunday");
+            break;
+        default:printf("NO SUCH DAY EXIST");
+
+    }
+    return 0;
+}
---
+++ switch/concept.c
@@ -0,0 +1,14 @@
+/*
+switch(number){
+    case c1:do something
+    break;
+    case c2:do something
+    break;
+    default:do something
+}
+
+break is used to break the switch
+
+default is used to print if no switch is worked
+
+*/
